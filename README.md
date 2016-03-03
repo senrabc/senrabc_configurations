e3368b26ede1d29d4ef7725a6ac82628441dd077
Christopher Barnes <senrabc@gmail.com>
2016-03-03 12:27:29 -0500

    update brew and other install items

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # senrabc_configurations
-Personalizations, Localizations, useful information I don't want to forget
+Personalizations, Localizations, useful information I don't want to forget.
+
+#
--- brew_install.sh
+++ brew_install.sh
@@ -1,5 +1,10 @@
 # stuff to install with brew to setup your mac
 
+#install xcode from the app store first (required)
+
+#install xcode commandline tools
+xcode-select --install
+
 
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -7,8 +12,22 @@
 #brew install wget
 brew install wget
 
-# you need cask for cask apps like atom
-brew install caskroom/cask/brew-cask
+#atom text editor rocks 
+brew cask install atom
 
+# I like virtual machines
+brew cask install vagrant
 
+# I like running virtual machines more
+brew cask install virtualbox
 
+# brew maint.
+# helps free up space and solves most errors
+
+brew update
+
+brew cask update
+
+brew cleanup
+
+brew cask cleanup

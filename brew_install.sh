abcc6f9a4d598e6742115c811f6312d54fe03515
Christopher Barnes <senrabc@gmail.com>
2016-03-03 09:55:26 -0500

    add all my brew packages to keep track of setting up a mac

---
+++ brew_install.sh
@@ -0,0 +1,14 @@
+# stuff to install with brew to setup your mac
+
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#brew install wget
+brew install wget
+
+# you need cask for cask apps like atom
+brew install caskroom/cask/brew-cask
+
+
+
